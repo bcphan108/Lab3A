ba3000a51305fc30b592e999420a05b66b495e12
Brian Cong Luan Phan <luan@lnxsrv09.seas.ucla.edu>
2020-03-02 15:33:11 -0800

    Added group outputs

--- lab3a.c
+++ lab3a.c
@@ -10,6 +10,7 @@
 int fd;
 const int SUPERBLOCK_VALUE = 1024;
 struct ext2_super_block super_block;
+struct ext2_group_desc* groups;
 
 void printerror(char *mess)
 {
@@ -20,6 +21,8 @@ void printerror(char *mess)
 int main(int argc, char* argv[])
 {
     int ret_result = 0;
+    unsigned int block_size;
+    int true_block_num;
 
     if (argc != 2)
         printerror("Incorrect syntax, use: ./lab3a filename\n");
@@ -33,11 +36,34 @@ int main(int argc, char* argv[])
     if (ret_result < 0)
         printerror("Error reading superblock");
 
+
     //super block output
+    block_size = EXT2_MIN_BLOCK_SIZE << super_block.s_log_block_size;
+
     printf("SUPERBLOCK,%u,%u,%u,%u,%u,%u,%u\n", super_block.s_blocks_count,
-        super_block.s_inodes_count, SUPERBLOCK_VALUE, super_block.s_inode_size,
-        super_block.s_blocks_per_group, super_block.s_inodes_per_group, 
-        super_block.s_first_ino);
+        super_block.s_inodes_count, block_size, 
+        super_block.s_inode_size, super_block.s_blocks_per_group, 
+        super_block.s_inodes_per_group, super_block.s_first_ino);
+
+
+
+    //print group info
+    groups = malloc(sizeof(struct ext2_group_desc));
+    if (groups == NULL)
+        printerror("Error allocating memory for structure\n");
+
+    int offset_tmp = SUPERBLOCK_VALUE + (int) block_size; //* sizeof(struct ext2_group_desc);
+    ret_result = pread(fd, &groups[0], sizeof(struct ext2_group_desc), offset_tmp);
+    if (ret_result < 0)
+        printerror("Error reading groups\n");
+
+    if (super_block.s_blocks_per_group > super_block.s_blocks_count)
+        true_block_num = super_block.s_blocks_count;
+    else
+        true_block_num = super_block.s_blocks_per_group;
+    printf("GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n", 0, true_block_num, super_block.s_inodes_per_group,
+        groups[0].bg_free_blocks_count, groups[0].bg_free_inodes_count,
+        groups[0].bg_block_bitmap, groups[0].bg_inode_bitmap, groups[0].bg_inode_table);
 
 
     exit(0);
