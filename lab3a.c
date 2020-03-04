e847c76f081ec138404fa44bd925b4acbf73047d
Brian Cong Luan Phan <luan@lnxsrv06.seas.ucla.edu>
2020-03-03 18:37:27 -0800

    Minor changes

--- lab3a.c
+++ lab3a.c
@@ -32,12 +32,12 @@ int main(int argc, char* argv[])
     if (fd < 0)
         printerror("Error opening file\n");
 
-    ret_result = pread(fd, &super_block, sizeof(super_block), SUPERBLOCK_VALUE);
-    if (ret_result < 0)
-        printerror("Error reading superblock");
 
 
     //super block output
+    ret_result = pread(fd, &super_block, sizeof(super_block), SUPERBLOCK_VALUE);
+    if (ret_result < 0)
+        printerror("Error reading superblock");
     block_size = EXT2_MIN_BLOCK_SIZE << super_block.s_log_block_size;
 
     printf("SUPERBLOCK,%u,%u,%u,%u,%u,%u,%u\n", super_block.s_blocks_count,
@@ -48,11 +48,15 @@ int main(int argc, char* argv[])
 
 
     //print group info
+    int x;
     groups = malloc(sizeof(struct ext2_group_desc));
     if (groups == NULL)
         printerror("Error allocating memory for structure\n");
-
-    int offset_tmp = SUPERBLOCK_VALUE + (int) block_size; //* sizeof(struct ext2_group_desc);
+    if (block_size == 1024)
+        x = 2;
+    else
+        x = 1;
+    int offset_tmp = block_size * x; //* sizeof(struct ext2_group_desc);
     ret_result = pread(fd, &groups[0], sizeof(struct ext2_group_desc), offset_tmp);
     if (ret_result < 0)
         printerror("Error reading groups\n");
@@ -64,6 +68,30 @@ int main(int argc, char* argv[])
     printf("GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n", 0, true_block_num, super_block.s_inodes_per_group,
         groups[0].bg_free_blocks_count, groups[0].bg_free_inodes_count,
         groups[0].bg_block_bitmap, groups[0].bg_inode_bitmap, groups[0].bg_inode_table);
+
+    //bitmap
+
+    char* bitmap = malloc(block_size * sizeof(char));
+    offset_tmp = groups[0].bg_block_bitmap * block_size;
+    ret_result = pread(fd, bitmap, block_size, offset_tmp);
+    if (ret_result < 0)
+        printerror("Error reading bitmap\n");
+    unsigned int i;
+    int j;
+    char byte;
+    for (i = 0; i < block_size; i++)
+    {
+        byte = bitmap[i];
+        for (j = 0; j < 8; j++)
+        {
+            int bit = byte & (1 << j);
+            int number = 0 * super_block.s_blocks_per_group + i * 8 + j + 1;
+            if (!bit)
+            {
+                printf("BFREE,%u\n", number);
+            }
+        }
+    }
 
 
     exit(0);
